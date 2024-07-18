bbdfdcfb1083da8f7a7f2d7f83b7ce1e0e9dbd9b
Paul Ramon <peramon98.18@gmail.com>
2024-07-18 17:23:25 -0500

    Add properties conf

--- src/main/java/com/lojagas/odometers/OdometersApplication.java
+++ src/main/java/com/lojagas/odometers/OdometersApplication.java
@@ -2,10 +2,15 @@ package com.lojagas.odometers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class OdometersApplication {
 
+	@GetMapping("/")
+	String home() { return "Odometers Service";}
 	public static void main(String[] args) {
 		SpringApplication.run(OdometersApplication.class, args);
 	}
