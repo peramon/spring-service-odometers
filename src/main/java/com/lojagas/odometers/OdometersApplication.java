e3f09881787ea47b633ef72c884ef6757ed618d6
Paul Ramon <peramon98.18@gmail.com>
2024-07-18 10:01:04 -0500

    create structure of proyect

---
+++ src/main/java/com/lojagas/odometers/OdometersApplication.java
@@ -0,0 +1,13 @@
+package com.lojagas.odometers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdometersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdometersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lojagas/odometers/OdometersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lojagas.odometers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdometersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
